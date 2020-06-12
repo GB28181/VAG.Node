2b0ddb219682b03f82287533c8518b8f056458fe
yzxliu <89369553@qq.com>
2020-06-12 19:20:49 +0800

    修复bug

--- src/node_core_ctx.js
+++ src/node_core_ctx.js
@@ -5,12 +5,11 @@ let sessions = new Map();
 
 let publishers = new Map();//RTMP推流客户端
 
-
-
 let nodeEvent = new EventEmitter();
+
 let stat = {
   inbytes: 0,
   outbytes: 0,
   accepted: 0
 };
-module.exports = { sessions, publishers, idlePlayers, nodeEvent, stat };
+module.exports = { sessions, publishers, nodeEvent, stat };
