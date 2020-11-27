2f5131f4ce19d62c331ff927c98ca4a8bd283280
yzxliu <35661406+yzxliu@users.noreply.github.com>
2020-11-27 13:46:12 +0800

    Update GB28181Server.js
    少个this.id

--- GB28181Server.js
+++ GB28181Server.js
@@ -10,6 +10,7 @@ class NodeSIPServer {
         this.listen = config.GB28181.sipServer.listen || 5060;
         this.host = config.GB28181.sipServer.host || '127.0.0.1';//SIP服务器主机地址
         this.defaultPassword = config.GB28181.sipServer.password || '12345678';
+        this.id=config.GB28181.sipServer.serial||'34020000002000000001';
         this.config = config;
         //临时用户信息
         this.userinfo = {};
